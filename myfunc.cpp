fd2c2f880630a9df4d9e7fe023c15dd7feced254
ChristopherStaunton <55333540+ChristopherStaunton@users.noreply.github.com>
2019-09-14 18:21:23 -0400

    Add files via upload

---
+++ myfunc.cpp
@@ -0,0 +1,12 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Sep 11, 2019
+ *      Author: Christopher Staunton
+ */
+#include "myfunc.h"
+
+std::string func()
+{
+	return "hello";
+}
---
+++ myfunc.h
@@ -0,0 +1,16 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 11, 2019
+ *      Author: Christopher Staunton
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <string>
+
+
+std::string func();
+
+
+#endif /* MYFUNC_H_ */
